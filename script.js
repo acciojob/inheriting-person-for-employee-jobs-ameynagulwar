3d92009620fbf3ff2b2b6d430cf1bfadd325af6c
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-29 10:17:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
+// 'Hello, my name is Alice and I am 25 years old.'
 Person.prototype.greet = function() {
-  console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
+  console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
 };
 
 function Employee(name, age, jobTitle) {
@@ -16,7 +16,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
+  console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 };
 
 // Do not change code below this line
